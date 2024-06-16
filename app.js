4155eaae28764c4fc08b816f77d1cb378526c992
Alekrish-12 <vtu12706@veltech.edu.in>
2024-06-17 02:45:06 +0530

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-// Example fetch API usage to get products
-fetch('/products')
-  .then(response => response.json())
-  .then(products => {
-    // Display products on the webpage
-    console.log(products);
-  })
-  .catch(error => console.error('Error fetching products:', error));
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productsRouter = require('./routes/products');
+const suppliersRouter = require('./routes/suppliers');
+const warehousesRouter = require('./routes/warehouses');
+const stockLevelsRouter = require('./routes/stockLevels');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/products', productsRouter);
+app.use('/suppliers', suppliersRouter);
+app.use('/warehouses', warehousesRouter);
+app.use('/stock_levels', stockLevelsRouter);
+
+mongoose.connect('mongodb://localhost:27017/inventory-management', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
